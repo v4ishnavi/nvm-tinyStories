fac2900de25a70ab72aa79d2c4d9bac2ca9c423a
cookiepresence <127502417+cookiepresence@users.noreply.github.com>
2024-10-07 02:41:47 +0530

    feat: add some scripts to make life easy
    this adds scripts that make dealing with ada easier.
    
    ada-sync.sh makes synchronization of code simpler, without having to
    do a whole cycle of git commit-push-pull, while also having a local
    copy of the code.
    
    ada-training-run.sh sets up a training run using slurm, so that you
    dont need to carry around your laptop everywhere to prevent your
    interactive job from never starting.

---
+++ scripts/ada-sync.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Syncs current folder with the folders on Ada. This makes it easier
+# to develop, since both share a single source of truth.
+
+# Use -s to ssh directly, instead of having a separate process to ssh
+
+# NOTE: Make sure to modify this as per your convienience
+# In particular, change the path of the files, as well as the shell.
+# This file is written keeping my preferences in mind.
+
+REMOTE_PATH="anlp/project"
+USERNAME="monish"
+
+# Ensures that all files, excluding the venv and artifacts, are synced
+# with Ada.
+rsync -chavzP --stats ../ ada:/home/$USERNAME/$REMOTE_PATH --exclude "venv" --exclude "artifacts"
+
+case "$1" in
+    -s) ssh -t ada 'cd ~/anlp/$REMOTE_PATH; zsh -l'
+esac
