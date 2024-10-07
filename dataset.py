7c55a6e7a7120f6468d4e4bb65f50b30a9907a91
nandarajiv <nanda.rajiv@research.iiit.ac.in>
2024-10-08 00:09:09 +0530

    indentation shift tabbed in dataset

--- dataset.py
+++ dataset.py
@@ -65,100 +65,95 @@ def create_dataloader_from_file(
 
         train_stories.append(story)
 
-        for i in tqdm(
-            range(int(fraction_wanted * len(val_data))), desc="Processing val data"
-        ):
-            story = val_data[i]["text"]
-            story = story.lower()
-            story = re.sub(r"[^a-zA-Z\s]", " ", story)
-            story = re.sub(r"\s+", " ", story)
+    for i in tqdm(
+        range(int(fraction_wanted * len(val_data))), desc="Processing val data"
+    ):
+        story = val_data[i]["text"]
+        story = story.lower()
+        story = re.sub(r"[^a-zA-Z\s]", " ", story)
+        story = re.sub(r"\s+", " ", story)
 
-            story = story.strip()
-            story = nltk.word_tokenize(story)
-            story = ["<bos>"] + story + ["<eos>"]
+        story = story.strip()
+        story = nltk.word_tokenize(story)
+        story = ["<bos>"] + story + ["<eos>"]
 
-            for word in story:
-                if word in word_count:
-                    word_count[word] += 1
-                else:
-                    word_count[word] = 1
+        for word in story:
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
 
-            val_stories.append(story)
+        val_stories.append(story)
 
-        if vocab_file is not None:
-            with open(vocab_file, "rb") as f:
-                vocab = pickle.load(f)
+    if vocab_file is not None:
+        with open(vocab_file, "rb") as f:
+            vocab = pickle.load(f)
+    else:
+        vocab = {}
+        vocab["<bos>"] = len(vocab)
+        vocab["<eos>"] = len(vocab)
+        vocab["<pad>"] = len(vocab)
+        vocab["<unk>"] = len(vocab)
+
+        for data in [train_stories, val_stories]:
+            for story in data:
+                for word in story:
+                    if word_count[word] >= vocab_threshold and word not in vocab.keys():
+                        vocab[word] = len(vocab)
+
+        with open("vocab.pkl", "wb") as f:
+            pickle.dump(vocab, f)
+
+    idx2word = {idx: word for word, idx in vocab.items()}
+
+    index_train_stories = []
+    padded_train_stories = []
+    index_val_stories = []
+    padded_val_stories = []
+
+    for story in tqdm(train_stories, desc="Encoding train stories"):
+        index_story = [vocab.get(word, vocab["<unk>"]) for word in story]
+        new_story = story[:max_seq_length]
+        new_story = new_story + ["<pad>"] * (max_seq_length - len(new_story))
+
+        if len(index_story) > max_seq_length:
+            index_story = index_story[:max_seq_length]
         else:
-            vocab = {}
-            vocab["<bos>"] = len(vocab)
-            vocab["<eos>"] = len(vocab)
-            vocab["<pad>"] = len(vocab)
-            vocab["<unk>"] = len(vocab)
+            index_story = index_story + [vocab["<pad>"]] * (
+                max_seq_length - len(index_story)
+            )
 
-            for data in [train_stories, val_stories]:
-                for story in data:
-                    for word in story:
-                        if (
-                            word_count[word] >= vocab_threshold
-                            and word not in vocab.keys()
-                        ):
-                            vocab[word] = len(vocab)
+        index_train_stories.append(index_story)
+        padded_train_stories.append(new_story)
 
-            with open("vocab.pkl", "wb") as f:
-                pickle.dump(vocab, f)
+    for story in tqdm(val_stories, desc="Encoding val stories"):
+        index_story = [vocab.get(word, vocab["<unk>"]) for word in story]
+        new_story = story[:max_seq_length]
+        new_story = new_story + ["<pad>"] * (max_seq_length - len(new_story))
 
-        idx2word = {idx: word for word, idx in vocab.items()}
+        if len(index_story) > max_seq_length:
+            index_story = index_story[:max_seq_length]
+        else:
+            index_story = index_story + [vocab["<pad>"]] * (
+                max_seq_length - len(index_story)
+            )
 
-        index_train_stories = []
-        padded_train_stories = []
-        index_val_stories = []
-        padded_val_stories = []
+        index_val_stories.append(index_story)
+        padded_val_stories.append(new_story)
 
-        for story in tqdm(train_stories, desc="Encoding train stories"):
-            index_story = [vocab.get(word, vocab["<unk>"]) for word in story]
-            new_story = story[:max_seq_length]
-            new_story = new_story + ["<pad>"] * (max_seq_length - len(new_story))
+    train_dataset = Small_Transformers_Dataset(index_train_stories, vocab, idx2word)
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=train_batch_size, shuffle=False
+    )
 
-            if len(index_story) > max_seq_length:
-                index_story = index_story[:max_seq_length]
-            else:
-                index_story = index_story + [vocab["<pad>"]] * (
-                    max_seq_length - len(index_story)
-                )
+    val_dataset = Small_Transformers_Dataset(index_val_stories, vocab, idx2word)
+    val_dataloader = DataLoader(val_dataset, batch_size=val_batch_size, shuffle=False)
 
-            index_train_stories.append(index_story)
-            padded_train_stories.append(new_story)
-
-        for story in tqdm(val_stories, desc="Encoding val stories"):
-            index_story = [vocab.get(word, vocab["<unk>"]) for word in story]
-            new_story = story[:max_seq_length]
-            new_story = new_story + ["<pad>"] * (max_seq_length - len(new_story))
-
-            if len(index_story) > max_seq_length:
-                index_story = index_story[:max_seq_length]
-            else:
-                index_story = index_story + [vocab["<pad>"]] * (
-                    max_seq_length - len(index_story)
-                )
-
-            index_val_stories.append(index_story)
-            padded_val_stories.append(new_story)
-
-        train_dataset = Small_Transformers_Dataset(index_train_stories, vocab, idx2word)
-        train_dataloader = DataLoader(
-            train_dataset, batch_size=train_batch_size, shuffle=False
-        )
-
-        val_dataset = Small_Transformers_Dataset(index_val_stories, vocab, idx2word)
-        val_dataloader = DataLoader(
-            val_dataset, batch_size=val_batch_size, shuffle=False
-        )
-
-        return train_dataloader, val_dataloader
+    return train_dataloader, val_dataloader
 
 
 def main():
-    train_dataloader, val_dataloader = create_dataloder_from_file(
+    train_dataloader, val_dataloader = create_dataloader_from_file(
         "roneneldan/TinyStories", 512, 0.0005, 5, 64, 8
     )
 
@@ -172,35 +167,45 @@ def main():
         break
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Create the parser
     parser = argparse.ArgumentParser(description="Setup the dataset")
 
     # Allow for logging support throughout the app
     parser.add_argument(
-        '-d', '--debug',
+        "-d",
+        "--debug",
         help="Print lots of debugging statements",
-        action="store_const", dest="loglevel", const=logging.DEBUG,
+        action="store_const",
+        dest="loglevel",
+        const=logging.DEBUG,
         default=logging.WARNING,
     )
     parser.add_argument(
-        '-v', '--verbose',
+        "-v",
+        "--verbose",
         help="Be verbose",
-        action="store_const", dest="loglevel", const=logging.INFO,
+        action="store_const",
+        dest="loglevel",
+        const=logging.INFO,
     )
 
     parser.add_argument(
-        '-s', '--seed',
+        "-s",
+        "--seed",
         help="Force seed for deterministic randomness",
-        action="store", dest="seed"
+        action="store",
+        dest="seed",
     )
 
     parser.add_argument(
-        '--vocab-output',
+        "--vocab-output",
         help="Location of vocabulary file",
-        action="store", dest="vocab_output", default="artifacts/vocab.json"
+        action="store",
+        dest="vocab_output",
+        default="artifacts/vocab.json",
     )
-    
+
     # Parse the arguments
     args = parser.parse_args()
 
